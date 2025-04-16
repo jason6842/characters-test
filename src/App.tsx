e1bd2899e15476a605bd1154b630dfcea74132ca
Jason Ma <jasonma062019@gmail.com>
2025-04-16 00:20:25 -0700

    Completed basic testing for Character

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { CharacterType } from "./types/characterType";
 import { getCharacters } from "./api/get-characters";
 import Character from "./components/Character/Character";
-
+import characters from "./testing/mock/characters";
 export default function App(): JSX.Element {
   const [characters, setCharacters] = React.useState([] as CharacterType[]);
   const [pageNumber, setPageNumber] = React.useState(1 as number);
@@ -19,7 +19,8 @@ export default function App(): JSX.Element {
 
     fetchCharacters();
   }, [pageNumber]);
-
+  console.log(characters[0]);
+  
   return (
     <div>
       <h1>Characters</h1>
--- src/components/Character/Character.test.tsx
+++ src/components/Character/Character.test.tsx
@@ -1,9 +1,32 @@
 import { describe, expect, test } from "vitest";
 import Character from "./Character";
+import { render, screen } from "@testing-library/react";
+import characters from "../../testing/mock/characters";
+import '@testing-library/jest-dom'
+
+
 
 describe("<Character />", () => {
-  test('shows fields for "name" and "culture"', async () => {});
-  test("shows culture if it is present", async () => {});
-  test("shows alias if no name is present", async () => {});
-  test("shows how many books this characters made an appearance in", async () => {});
+  test('shows fields for "name" and "culture"', async () => {
+    render(<Character character={characters[0]}/>);
+    expect(screen.getByText("name")).toBeInTheDocument();
+    expect(screen.getByText("culture")).toBeInTheDocument();
+  });
+  test("shows culture if it is present", async () => {
+    render(<Character character={characters[0]} />);
+    expect(screen.getByText("Braavosi")).toBeInTheDocument();
+  });
+  test("shows alias if no name is present", async () => {
+    render(<Character character={characters[2]} />);
+    expect(screen.getByText("Lamprey")).toBeInTheDocument();
+  });
+  test("shows how many books this characters made an appearance in", async () => {
+    render(<Character character={characters[1]} />);
+    const element = screen.getByText(/Number of Books:/)
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe("STRONG");
+
+    const parent = element.closest("div");
+    expect(parent).toHaveTextContent("Number of Books: 5");
+  });
 });
