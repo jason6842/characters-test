7bd4ac9a257ae5dfa07832fd4fc4cc5850829c4d
Jason Ma <jasonma062019@gmail.com>
2025-04-16 10:21:55 -0700

    Completed given test cases for App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,15 +3,47 @@ import React from "react";
 import App from "./App";
 
 import { describe, expect, test } from "vitest";
+import characters from "./testing/mock/characters";
 
 describe("<App />", () => {
   describe("loading character", () => {
-    test('renders the title "Characters"', async () => {});
-    test("renders a list of 10 characters", async () => {});
+    test('renders the title "Characters"', async () => {
+      render(<App />);
+      expect(screen.getByText("Characters")).toBeInTheDocument();
+    });
+    test("renders a list of 10 characters", async () => {
+      render(<App />);
+      // any find() is a promise function that waits for the text to appear on the DOM, but get() expects the text to be there already
+      const characterElements = await screen.findAllByText("name");
+      expect(characterElements).toHaveLength(10);
+    });
   });
   describe("loading more characters", () => {
-    test('has a "Load More Characters" button', async () => {});
-    test("clicking load more gets 10 more characters", async () => {});
-    test("clicking load more increases the page number", async () => {});
+    test('has a "Load More Characters" button', async () => {
+      render(<App />);
+      const buttonElement = screen.getByRole("button", {
+        name: "Load More Characters",
+      });
+      expect(buttonElement).toBeInTheDocument();
+    });
+    test("clicking load more gets 10 more characters", async () => {
+      render(<App />);
+      const buttonElement = screen.getByRole("button", {
+        name: "Load More Characters",
+      });
+      fireEvent.click(buttonElement);
+
+      const secondPageFirstName = await screen.findByText("The waif");
+      expect(secondPageFirstName).toBeInTheDocument();
+    });
+    test("clicking load more increases the page number", async () => {
+      render(<App />);
+      const buttonElement = screen.getByRole("button", {
+        name: "Load More Characters",
+      });
+      fireEvent.click(buttonElement);
+      const pageNumberText = screen.getByText("Next Page: 2");
+      expect(pageNumberText).toBeInTheDocument();
+    });
   });
 });
